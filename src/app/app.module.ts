55b85fac60ac33ed9b335e0e19e1d95069a50240
Zac <zilson19878@gmail.com>
2022-01-17 19:12:13 -0600

    first commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoComponent } from './components/todos/todo/todo.component';
+import { EditTodoComponent } from './components/todos/todoForm/todoForm.component';
+import { TodoService } from './services/todo.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodosComponent,
+    TodoComponent,
+    EditTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
